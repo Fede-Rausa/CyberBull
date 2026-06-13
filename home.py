4110da884ce9d9c1af04f0164f5c04db5076249c
Fede-Rausa <126892947+Fede-Rausa@users.noreply.github.com>
2026-06-13 11:47:50 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,12 @@
 import streamlit as st
-
+from css_utils import css_style_str
 
 st.set_page_config(
     page_title="Social Graph",
     page_icon="👋",
 )
+
+st.markdown(css_style_str(), unsafe_allow_html=True)
 
 st.markdown(
     """
@@ -25,9 +27,30 @@ if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
 if not st.session_state.get("logged_in", False):
-    if st.button('Login or register Now'):
+    if st.button('Login or register Now', type="primary"):
         st.switch_page("pages/login_or_register.py")
 else:
     if st.button('Logout'):
         st.session_state.logged_in = False
 
+
+if st.toggle("about"):
+
+    st.markdown("""
+## About the code
+
+This project is built with Streamlit and Supabase, showcasing a simple social network app with user profiles, posts, and a social graph.
+        
+It includes a simple interactive graph based on networkx and pyvis.
+
+The source code is available on [GitHub](https://github.com/Fede-Rausa/CyberBull#cyberbull)
+""")
+    
+if st.toggle("disclaimer"):
+
+    st.markdown("""
+## Disclaimer
+
+This project is for educational purposes only and does not promote or endorse any form of cyberbullying or harassment.
+Your privacy gets here the best protection possible.
+""")
