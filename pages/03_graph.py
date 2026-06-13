930318e2ef24a947482f99039affd3604a995519
Fede-Rausa <126892947+Fede-Rausa@users.noreply.github.com>
2026-06-13 10:10:28 +0200

    Add files via upload

--- pages/03_graph.py
+++ pages/03_graph.py
@@ -1,18 +1,3 @@
-# import streamlit as st
-
-# st.set_page_config(page_title="Graph")
-
-
-# if "logged_in" not in st.session_state:
-#     st.session_state.logged_in = False
-
-# if not st.session_state.get("logged_in", False):
-#     st.switch_page("pages/login_or_register.py")
-
-
-# st.title("Graph of users")
-
-
 import streamlit as st
 import networkx as nx
 from pyvis.network import Network
